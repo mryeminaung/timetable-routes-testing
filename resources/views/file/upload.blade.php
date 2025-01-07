36c293377e418a07d4db174ae32b7e1597070e3c
mryeminaung <yeminaung013@gmail.com>
2025-01-08 04:00:45 +0630

    storing uploaded files to aws s3 bucket

---
+++ resources/views/file/upload.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-md-12">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+                <form action="{{ route('uploading') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('post')
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">
+                            <h3>Uploading File to S3 Bucket</h3>
+                        </label>
+                        <input class="form-control" name="myfile" type="file" id="formFile">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,31 @@
 <?php
 
 use App\Http\Controllers\FacultyController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('file-upload-to-s3', function () {
+    return view('file.upload');
+});
+
+Route::post('uploading', function (Request $request) {
+    if ($request->hasFile('myfile')) {
+        $file = $request->file('myfile')->store('projects', [
+            'disk' => 's3',
+            'visibility' => 'public'
+        ]);
+
+        Log::info('url =>' . 'https://special-project-3001.s3.ap-southeast-1.amazonaws.com/' . $file);
+        return redirect()->back()->with('message', 'File uploaded successfully!');
+    }
+
+    return redirect()->back()->with('message', 'File uploading Failed!');
+})->name('uploading');
 
 Route::get('faculties', [FacultyController::class, 'index'])->name('faculties.index');
 
